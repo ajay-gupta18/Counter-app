7d6f279010992def267c47496d2aa9b43ea9e600
ajay-gupta18 <ajgupta@bestpeers.com>
2024-10-19 14:51:22 +0000

    functionality added : disable reset button when count is 0

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,24 +1,27 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Counter() {
 
+  const [resetDisable, setResetDisable] = useState(true);
   const [count, setCount] = useState(() => {
     const savedCount = localStorage.getItem('count');
-    return savedCount ? JSON.parse(savedCount) : 0; 
+    return savedCount ? JSON.parse(savedCount) : 0;
   });
 
   useEffect(() => {
     localStorage.setItem('count', JSON.stringify(count));
   }, [count]);
 
-
-  const handleIncrement=()=>{
+  const handleIncrement = () => {
+    setResetDisable(false)
     setCount(count + 1)
   }
-  const handleDecrement=()=>{
+  const handleDecrement = () => {
     setCount(count - 1)
+    setResetDisable(false)
   }
-  const handleReset=()=>{
+  const handleReset = () => {
+    setResetDisable(true)
     setCount(0)
   }
 
@@ -27,7 +30,7 @@ function Counter() {
       <p>{count}</p>
       <button onClick={handleIncrement}>+</button>
       <button onClick={handleDecrement}>-</button>
-      <button onClick={handleReset}>reset</button>
+      <button onClick={handleReset} disabled={resetDisable}>reset</button>
     </div>
   )
 }
